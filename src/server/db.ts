e7306335ea1a92637bef34580e80d89e275c47d1
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-05-29 00:52:33 +0100

    trying a solution for prisma to work on serverless

--- src/server/db.ts
+++ src/server/db.ts
@@ -6,6 +6,11 @@ const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
 const globalForPrisma = globalThis as unknown as {
