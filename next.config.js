95b20fb2031682b74fe497c166aeafe1a79c9104
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-05-29 00:16:24 +0100

    removing node env variable from next.config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ await import("./src/env.js");
 const config = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
