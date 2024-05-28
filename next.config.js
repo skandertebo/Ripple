a0404da9c5c66fd825cfbe19719ce977aa387c68
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-05-29 00:13:14 +0100

    fix: added env variables in next config for amplify sake

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
+};
 
 export default config;
