e537e87f8a233f2d7ce3f84d68d1ce340a24a469
melek <mbensnoussi@gmail.com>
2024-06-03 19:29:33 +0100

    fixed home page links

--- src/app/_components/homePage/homeMainComponent.tsx
+++ src/app/_components/homePage/homeMainComponent.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 export function HomeMainComponent() {
   return (
-    <div className="absolute -z-10 w-full p-4 md:left-[20%] md:top-[30%] md:w-[60%] md:p-0">
+    <div className="absolute z-10 w-full p-4 md:left-[20%] md:top-[30%] md:w-[60%] md:p-0">
       <p className="mb-4 text-3xl font-semibold md:text-5xl">
         Unlock Your Competitive Edge With
       </p>
       <p className="text-3xl font-bold text-primary md:text-6xl">Ripple</p>
       <div className="mt-16 flex w-full flex-row justify-start gap-4 text-xl md:gap-24 md:text-4xl">
         <Link
-          href={"/"}
-          className="text-white bg-black flex cursor-pointer flex-row rounded-3xl px-5 py-2 font-semibold"
+          href={"/influencers"}
+          className="flex cursor-pointer flex-row rounded-3xl bg-black px-5 py-2 font-semibold text-white"
         >
           <Image
             src="/cercleBlack.png"
@@ -22,8 +22,8 @@ export function HomeMainComponent() {
           <p className="w-fit">Get Started</p>
         </Link>
         <Link
-          href={"/"}
-          className="text-white bg-secondary flex cursor-pointer flex-row rounded-3xl px-5 py-2 font-semibold"
+          href={"/learnMore"}
+          className="flex cursor-pointer flex-row rounded-3xl bg-secondary px-5 py-2 font-semibold text-white"
         >
           <Image
             src="/cercleGray.png"
